9256ddb37d21b3ea1ad85efe751ae152be02a8f6
johnmart19 <johnivan19999@gmail.com>
2025-02-10 10:27:15 +0200

    sm8250-common: Update OMX Dependencies for latest sources
    FAILED:
    build/make/core/main.mk:1101: warning:  device/xiaomi/alioth/lineage_alioth.mk includes non-existent modules in PRODUCT_PACKAGES
    Offending entries:
    libOmxAacEnc
    libOmxAmrEnc
    libOmxEvrcEnc
    libOmxG711Enc
    libOmxQcelp13Enc
    build/make/core/main.mk:1101: error: Build failed.

--- kona.mk
+++ kona.mk
@@ -299,12 +299,7 @@ endif
 
 # OMX
 PRODUCT_PACKAGES += \
-    libOmxAacEnc \
-    libOmxAmrEnc \
     libOmxCore \
-    libOmxEvrcEnc \
-    libOmxG711Enc \
-    libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
     libstagefrighthw
