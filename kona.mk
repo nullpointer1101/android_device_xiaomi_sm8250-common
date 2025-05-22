a5847fe06d2105a9037fc09e4ba96914ad94fef5
Pranshu Sheoran <sheoranpranshu@gmail.com>
2025-05-22 03:37:33 +0000

    sm8250-common: Set TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED and INIT_VENDOR_LIB via soong config warning: TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED is deprecated, please migrate to soong_config_set,camera,override_format_from_reserved warning: TARGET_INIT_VENDOR_LIB is deprecated, please migrate to soong_config_set,libinit,vendor_init_lib

--- kona.mk
+++ kona.mk
@@ -138,6 +138,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Camera
+$(call soong_config_set,camera,override_format_from_reserved,true)
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
@@ -244,6 +245,9 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 $(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
+# Libinit
+$(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_kona)
 
 # Media configs
 PRODUCT_PACKAGES += \
